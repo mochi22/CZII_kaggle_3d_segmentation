18ec568bbfcb2c5d578e47e9514007df57f1e3fd
kuraryu <kuraryu@amazon.com>
2025-01-01 05:52:33 +0000

    kuraryu kaggle template!

--- experiments/run.py
+++ experiments/run.py
@@ -10,7 +10,8 @@ from omegaconf import DictConfig, OmegaConf
 import utils
 
 
-@hydra.main(version_base=None, config_path=".", config_name="config")
+
+@hydra.main(version_base=None, config_path=".", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
     runtime_choices = HydraConfig.get().runtime.choices
     exp_name = f"{Path(sys.argv[0]).parent.name}/{runtime_choices.exp}"
@@ -20,8 +21,6 @@ def main(cfg: DictConfig) -> None:
     print(f"ouput_path: {output_path}")
     os.makedirs(output_path, exist_ok=True)
 
-    with utils.timer("print"):
-        time.sleep(1.1)
     print(OmegaConf.to_yaml(cfg))
 
 
